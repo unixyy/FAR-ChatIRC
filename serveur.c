b48d9bb11de5a4c05dd880611c611c4926b2da5c
Richard MARTIN <rclic66000@gmail.com>
2022-04-13 11:29:00 +0200

    serveur remaking
    Co-authored-by: Thomas Fournier <thmsfnr@users.noreply.github.com>
    Co-authored-by: jason.moret <Jasonzon@users.noreply.github.com>

--- serveur.c
+++ serveur.c
@@ -3,6 +3,72 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> 
+#include <pthread.h>
+
+#define NB_THREADS 2
+
+int stop = 0;
+
+void * receiveSend(void * dS, void * dSC){
+    char haveToStop[] = "fin";
+  while(1){
+      int taille;
+      int tailleRCV = recv(dSC, &taille, sizeof(int), 0);
+      if (tailleRCV == -1) {
+        perror("Erreur recv");
+        shutdown(dSC, 2) ; 
+        shutdown(dS, 2) ;
+        exit(0);
+      }
+      else if (tailleRCV == 0) {
+        break;
+      }
+      printf("taille reçu : %d\n", taille) ;
+
+      char *msg = (char *)malloc(sizeof(char)*taille);
+      int msgRCV = recv(dSC, msg, taille*sizeof(char), 0);
+      if (msgRCV == -1 ) {
+        perror("Erreur recv");
+          shutdown(dSC, 2) ; 
+      shutdown(dS, 2) ;
+        exit(0);
+      }
+      else if (msgRCV == 0) {
+        break;
+      }
+      printf("Message reçu : %s\n", msg) ;
+
+      strtok(msg,"\n");
+      strtok(haveToStop,"\0");
+      if (strcmp(msg, haveToStop) == 0) {
+        int tmp = stop;
+        tmp = 1;
+        stop = tmp;
+      }
+
+      if (send(dS, &taille, sizeof(int), 0) == -1) {
+        perror("Erreur send");
+        shutdown(dSC, 2) ; 
+        shutdown(dS,2) ;  
+        exit(0);
+      }
+      printf("Taille Envoyé \n");
+    
+      if (send(dS, msg, taille*sizeof(char), 0) == -1) {
+        perror("Erreur send");
+          shutdown(dSC, 2) ; 
+      shutdown(dS, 2) ;
+        exit(0);
+      }
+      printf("Message Envoyé\n");
+      free(msg);
+  }
+}
+
+
+
+
 
 int main(int argc, char *argv[]) {
   
@@ -28,139 +94,44 @@ int main(int argc, char *argv[]) {
 
   listen(dS, 7) ;
   printf("Mode écoute\n");
+  int *CliArray = calloc(20, sizeof(int));
+  int indexCli = 0;
+  while (1)
+  {
 
-  while (1) {
-
-        struct sockaddr_in aC ;
+    struct sockaddr_in aC;
     socklen_t lg = sizeof(struct sockaddr_in) ;
-    int dSC = accept(dS, (struct sockaddr*) &aC,&lg) ;
-    if (dSC == -1) {
-      perror("Erreur accept");
-    shutdown(dS, 2) ;
-      exit(0);
-    }
-    printf("Client Connecté\n");
-
-    int dSC2 = accept(dS, (struct sockaddr*) &aC,&lg) ;
-    if (dSC2 == -1) {
-      perror("Erreur accept");
-    shutdown(dS, 2) ;
-      exit(0);
-    }
-    printf("Client 2 Connecté\n");
-
-  int stop = 0;
-  char haveToStop[] = "fin";
-
-  while (!stop) {
-
-
-    int taille;
-    int tailleRCV = recv(dSC, &taille, sizeof(int), 0);
-    if (tailleRCV == -1) {
-      perror("Erreur recv");
-      shutdown(dSC, 2) ; 
+    if (indexCli >= 19){
+      printf("Trop de clients");
+      sleep(5);
+    }else{
+      int dSC = accept(dS, (struct sockaddr*) &aC,&lg) ;
+      if (dSC == -1) {
+        perror("Erreur accept");
       shutdown(dS, 2) ;
-      exit(0);
-    }
-    else if (tailleRCV == 0) {
-      break;
-    }
-    printf("taille reçu : %d\n", taille) ;
-
-    char *msg = (char *)malloc(sizeof(char)*taille);
-    int msgRCV = recv(dSC, msg, taille*sizeof(char), 0);
-    if (msgRCV == -1 ) {
-      perror("Erreur recv");
-         shutdown(dSC, 2) ; 
-    shutdown(dS, 2) ;
-      exit(0);
-    }
-    else if (msgRCV == 0) {
-      break;
-    }
-    printf("Message reçu : %s\n", msg) ;
-
-    strtok(msg,"\n");
-    strtok(haveToStop,"\0");
-    if (strcmp(msg, haveToStop) == 0) {
-      stop=1;
-    } 
-    
-
-    if (send(dSC2, &taille, sizeof(int), 0) == -1) {
-      perror("Erreur send");
-      shutdown(dSC, 2) ; 
-       shutdown(dS,2) ;  
-      exit(0);
-    }
-    printf("Taille Envoyé \n");
+        exit(0);
+      }else{
+        indexCli++;
+      }
+      printf("Client Connecté\n");
+      CliArray[indexCli] = dSC;
   
-    if (send(dSC2, msg, taille*sizeof(char), 0) == -1) {
-      perror("Erreur send");
-         shutdown(dSC, 2) ; 
-    shutdown(dS, 2) ;
-      exit(0);
     }
-    printf("Message Envoyé\n");
-    free(msg);
+    int i = 0;
 
-
-
-  int taille2;
-    int tailleRCV2 = recv(dSC2, &taille2, sizeof(int), 0);
-    if (tailleRCV2 == -1) {
-      perror("Erreur recv");
-      shutdown(dSC2, 2) ; 
-      shutdown(dS, 2) ;
-      exit(0);
+    pthread_t thread[NB_THREADS];
+    for (i = 0; i < NB_THREADS; i++)
+    {
+      pthread_create(&thread[i], NULL, receiveSend,(void *)dSC);
     }
-    else if (tailleRCV2 == 0) {
-      break;
+    for (i = 0; i < NB_THREADS; i++)
+    {
+      pthread_join(thread[i], NULL);
     }
-    printf("taille reçu : %d\n", taille2) ;
-
-    char *msg2 = (char *)malloc(sizeof(char)*taille2);
-    int msgRCV2 = recv(dSC2, msg2, taille2*sizeof(char), 0);
-    if (msgRCV2 == -1 ) {
-      perror("Erreur recv");
-         shutdown(dSC2, 2) ; 
-    shutdown(dS, 2) ;
-      exit(0);
-    }
-    else if (msgRCV2 == 0) {
-      break;
-    }
-    printf("Message reçu : %s\n", msg2) ;
-
-    strtok(msg,"\n");
-    strtok(haveToStop,"\0");
-    if (strcmp(msg2, haveToStop) == 0) {
-      stop=1;
-    } 
-
-    if (send(dSC, &taille2, sizeof(int), 0) == -1) {
-      perror("Erreur send");
-      shutdown(dSC, 2) ; 
-       shutdown(dS,2) ;  
-      exit(0);
-    }
-    printf("Taille Envoyé \n");
-  
-    if (send(dSC, msg2, taille2*sizeof(char), 0) == -1) {
-      perror("Erreur send");
-         shutdown(dSC, 2) ; 
-    shutdown(dS, 2) ;
-      exit(0);
-    }
-    printf("Message Envoyé\n");
-    free(msg2);
-
-  
   }
        shutdown(dSC, 2) ;
        shutdown(dSC2, 2) ;
-  }
+
   shutdown(dS, 2) ;
   printf("Fin du programme");
 
