f11ad3d526f06608e007631c82ddae8a10fcec39
Thomas Fournier <thomas42600.f@gmail.com>
2022-04-14 12:50:05 +0200

    app with threads

--- client.c
+++ client.c
@@ -3,16 +3,59 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
+void * Receive(int* dS) {
+  int taille2;
+  int stop = 0;
+  char haveToStop[] = "fin";
+
+  while (!stop) {
+
+    int tailleRCV = recv(*dS, &taille2, sizeof(int), 0);
+    if (tailleRCV == -1) {
+      perror("Erreur recv");
+      shutdown(*dS, 2) ; 
+      exit(0);
+    }
+    else if (tailleRCV == 0) {
+      break;
+    }
+    // printf("taille reçu : %d\n", taille2) ;
+
+    char *msg = (char *)malloc(sizeof(char)*taille2);
+    int msgRCV = recv(*dS, msg, taille2*sizeof(char), 0);
+    if (msgRCV == -1 ) {
+      perror("Erreur recv");
+         shutdown(*dS, 2) ; 
+      exit(0);
+    }
+    else if (msgRCV == 0) {
+      break;
+    }
+    printf("Message reçu : %s\n", msg) ;
+
+    strtok(msg,"\n");
+      if (strcmp(msg, haveToStop) == 0) {
+        stop=1;
+      } 
+
+    free(msg);
+
+  }
+
+  pthread_exit(0);
+}
 
 int main(int argc, char *argv[]) {
 
-  printf("Début programme\n");
+  // printf("Début programme\n");
   int dS = socket(PF_INET, SOCK_STREAM, 0);
   if (dS == -1) {
     perror("Erreur socket");
     return -1;
   }
-  printf("Socket Créé\n");
+  // printf("Socket Créé\n");
         struct sockaddr_in aS;
     aS.sin_family = AF_INET;
     inet_pton(AF_INET,argv[1],&(aS.sin_addr)) ;
@@ -24,16 +67,19 @@ int main(int argc, char *argv[]) {
       return -1;
     }
 
+  // printf("Socket Connecté\n");
+
   int stop = 0;
   char haveToStop[] = "fin";
 
+  pthread_t my_thread;
+  pthread_create(&my_thread, NULL, Receive, &dS);
+
   while (!stop) {
 
-
-    printf("Socket Connecté\n");
     char *m = (char *)malloc(sizeof(char)*30);
     
-    printf("Quel est votre nom ? ");
+    printf("message : ");
     fgets(m, sizeof(char)*30, stdin);
 
     int taille = strlen(m);
@@ -42,54 +88,22 @@ int main(int argc, char *argv[]) {
        shutdown(dS,2) ;  
       exit(0);
     }
-    printf("Taille Envoyé \n");
+    // printf("Taille Envoyé \n");
 
     if (send(dS, m, strlen(m) , 0) == -1) {
       perror("Erreur send");
        shutdown(dS,2) ;  
       exit(0);
     }
-    printf("Message Envoyé \n");
+    // printf("Message Envoyé \n");
 
     strtok(m,"\n");
     strtok(haveToStop,"\0");
     if (strcmp(m, haveToStop) == 0) {
       stop=1;
     } 
-
-    int taille2;
-    int tailleRCV = recv(dS, &taille2, sizeof(int), 0);
-    if (tailleRCV == -1) {
-      perror("Erreur recv");
-      shutdown(dS, 2) ; 
-      exit(0);
-    }
-    else if (tailleRCV == 0) {
-      break;
-    }
-    printf("taille reçu : %d\n", taille2) ;
-
-    char *msg = (char *)malloc(sizeof(char)*taille);
-    int msgRCV = recv(dS, msg, taille2*sizeof(char), 0);
-    if (msgRCV == -1 ) {
-      perror("Erreur recv");
-         shutdown(dS, 2) ; 
-      exit(0);
-    }
-    else if (msgRCV == 0) {
-      break;
-    }
-    printf("Message reçu : %s\n", msg) ;
-
-  strtok(msg,"\n");
-    if (strcmp(msg, haveToStop) == 0) {
-      stop=1;
-    } 
-
   free(m);
-  free(msg);
-
   }
-      shutdown(dS,2) ;  
-    printf("Fin du programme");
+    shutdown(dS,2) ;  
+    // printf("Fin du programme");
 }
--- serveur.c
+++ serveur.c
@@ -8,30 +8,38 @@
 
 #define NB_THREADS 2
 
+struct data {
+    int dS;
+    int arrayCli[20];
+    int indexCli;
+  }datas;
 int stop = 0;
 
-void * receiveSend(void * dS, void * dSC){
+typedef struct data data;
+
+void * receiveSend(data* datas){
     char haveToStop[] = "fin";
+    int index = (*datas).indexCli-1;
   while(1){
       int taille;
-      int tailleRCV = recv(dSC, &taille, sizeof(int), 0);
+      int tailleRCV = recv((*datas).arrayCli[index], &taille, sizeof(int), 0);
       if (tailleRCV == -1) {
         perror("Erreur recv");
-        shutdown(dSC, 2) ; 
-        shutdown(dS, 2) ;
+        shutdown((*datas).arrayCli[index], 2) ; 
+        shutdown((*datas).dS, 2) ;
         exit(0);
       }
       else if (tailleRCV == 0) {
         break;
       }
-      printf("taille reçu : %d\n", taille) ;
+      // printf("taille reçu : %d\n", taille) ;
 
       char *msg = (char *)malloc(sizeof(char)*taille);
-      int msgRCV = recv(dSC, msg, taille*sizeof(char), 0);
+      int msgRCV = recv((*datas).arrayCli[index], msg, taille*sizeof(char), 0);
       if (msgRCV == -1 ) {
         perror("Erreur recv");
-          shutdown(dSC, 2) ; 
-      shutdown(dS, 2) ;
+          shutdown((*datas).arrayCli[index], 2) ; 
+      shutdown((*datas).dS, 2) ;
         exit(0);
       }
       else if (msgRCV == 0) {
@@ -47,23 +55,31 @@ void * receiveSend(void * dS, void * dSC){
         stop = tmp;
       }
 
-      if (send(dS, &taille, sizeof(int), 0) == -1) {
+      for (int i=0;i<(*datas).indexCli;i++) {
+        if (send(datas->arrayCli[i], &taille, sizeof(int), 0) == -1) {
         perror("Erreur send");
-        shutdown(dSC, 2) ; 
-        shutdown(dS,2) ;  
+        shutdown((*datas).arrayCli[index], 2) ; 
+        shutdown((*datas).dS,2) ;  
         exit(0);
       }
-      printf("Taille Envoyé \n");
-    
-      if (send(dS, msg, taille*sizeof(char), 0) == -1) {
+      // printf("Taille Envoyé \n");
+
+      }
+
+      
+    for (int i=0;i<(*datas).indexCli;i++) {
+      if (send(datas->arrayCli[i], msg, taille*sizeof(char), 0) == -1) {
         perror("Erreur send");
-          shutdown(dSC, 2) ; 
-      shutdown(dS, 2) ;
+          shutdown((*datas).arrayCli[index], 2) ; 
+      shutdown((*datas).dS, 2) ;
         exit(0);
       }
       printf("Message Envoyé\n");
-      free(msg);
+      
   }
+  free(msg);
+  }
+  pthread_exit(0);
 }
 
 
@@ -94,45 +110,37 @@ int main(int argc, char *argv[]) {
 
   listen(dS, 7) ;
   printf("Mode écoute\n");
-  int *CliArray = calloc(20, sizeof(int));
-  int indexCli = 0;
-  while (1)
-  {
+
+  
+
+  datas.dS = dS;
+  datas.indexCli = 0;
 
     struct sockaddr_in aC;
     socklen_t lg = sizeof(struct sockaddr_in) ;
-    if (indexCli >= 19){
+  while (1)
+  {
+    if (datas.indexCli >= 19){
       printf("Trop de clients");
       sleep(5);
     }else{
-      int dSC = accept(dS, (struct sockaddr*) &aC,&lg) ;
-      if (dSC == -1) {
+      datas.arrayCli[datas.indexCli] = accept(dS, (struct sockaddr*) &aC,&lg) ;
+      if (datas.arrayCli[datas.indexCli] == -1) {
         perror("Erreur accept");
       shutdown(dS, 2) ;
         exit(0);
       }else{
-        indexCli++;
+        datas.indexCli++;
       }
       printf("Client Connecté\n");
-      CliArray[indexCli] = dSC;
   
     }
-    int i = 0;
 
     pthread_t thread[NB_THREADS];
-    for (i = 0; i < NB_THREADS; i++)
-    {
-      pthread_create(&thread[i], NULL, receiveSend,(void *)dSC);
-    }
-    for (i = 0; i < NB_THREADS; i++)
-    {
-      pthread_join(thread[i], NULL);
-    }
-  }
-       shutdown(dSC, 2) ;
-       shutdown(dSC2, 2) ;
+    pthread_create(&thread[datas.indexCli], NULL, receiveSend, &datas);
 
-  shutdown(dS, 2) ;
+  }
+  shutdown(datas.dS, 2) ;
   printf("Fin du programme");
 
   
